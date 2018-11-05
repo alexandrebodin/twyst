99e71430d223a1dc86773935e02c8d1ba5095a3c
Alexandre BODIN <bodin.alex@gmail.com>
2018-11-05 08:47:13 +0100

    wip

--- packages/twyst-react/src/twyst.js
+++ packages/twyst-react/src/twyst.js
@@ -24,11 +24,50 @@ export class Subscribe extends React.Component {
 }
 
 export class Provider extends React.Component {
+  componentDidMount() {
+    this.unsubscribe = this.props.store.subscribe(() => this.setState({}));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
   render() {
     return (
       <React.Fragment>
-        <ctx.Provider>{this.props.children}</ctx.Provider>
+        <ctx.Provider value={this.props.store}>
+          {this.props.children}
+        </ctx.Provider>
       </React.Fragment>
     );
   }
+}
+
+// TODO: Implement a shallow compare to avoid to many rerenders
+export function connect(mapStateToProps, actions) {
+  return function Wrap(Cmp) {
+    function WrappedComponent({ children, ...props }) {
+      return (
+        <Consumer>
+          {store => {
+            const finalProps = {
+              ...props,
+              ...mapStateToProps(store.getState(), props)
+            };
+
+            return (
+              <Cmp {...finalProps} {...bindActions(store, actions)}>
+                {children}
+              </Cmp>
+            );
+          }}
+        </Consumer>
+      );
+    }
+
+    WrappedComponent.displayName =
+      Cmp.displayName || Cmp.name || `TwystConnected`;
+
+    return WrappedComponent;
+  };
 }
